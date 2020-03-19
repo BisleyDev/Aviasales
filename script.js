adfc69f30be2b38ce44238290a594defb4628a73
BisleyDev <andrey.bisley@yahoo.com>
2020-03-19 02:57:35 +0200

    First day and HW

--- script.js
+++ script.js
@@ -36,20 +36,27 @@ const showCity = (input, list) => {
 };
 
 
+const downList = (city, list) => {
+        //выбрать из списка подсказки кликом
+    list.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target.tagName.toLowerCase() === 'li') {
+            city.value = target.textContent;
+            list.textContent = '';
+        }
+    });
+};
+
+
+
 inputCitiesFrom.addEventListener('input', () => {
     showCity(inputCitiesFrom, dropdownCitiesFrom);
+    downList(inputCitiesFrom, dropdownCitiesFrom);
 });
-
-//выбрать из списка подсказки кликом
-dropdownCitiesFrom.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesFrom.value = target.textContent;
-        dropdownCitiesFrom.textContent = '';
-    }
+inputCitiesTo.addEventListener('input', () => {
+    showCity(inputCitiesTo, dropdownCitiesTo);
+    downList(inputCitiesTo, dropdownCitiesTo);
 });
-
-
 
 
 
